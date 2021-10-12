3301fed0e5263284b736d3a30d7660d4bcaba8fe
Davorin <davorin.jurkovic@gmail.com>
2021-10-12 15:21:55 +0200

    Initial Polly

---
+++ Polly/PollyDemo/Controllers/InventoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PollyDemo.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/Inventory")]
+    public class InventoryController : ControllerBase
+    {
+        static int requestCount = 0;
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            await Task.Delay(100); // simulate some data processing by delaying for 100 miliseconds
+            requestCount++;
+
+            if(requestCount % 4 == 0) // only one of out four request will succeed
+            {
+                return Ok(15);
+            }
+
+            return StatusCode((int)HttpStatusCode.InternalServerError, "Something went wrong");
+        }
+    }
+}
